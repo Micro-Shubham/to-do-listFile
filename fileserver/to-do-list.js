ac0e219b065e95f98a815e6a2c1a3c2988e38cb0
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-12-25 18:57:10 +0530

    to-do-list

--- fileserver/to-do-list.js
+++ fileserver/to-do-list.js
@@ -22,6 +22,44 @@ app.post("/post/:id", function (req, res) {
     })
 })
 
+app.put("/put/:id", function (req, res) {
+    const allitem = db.read();
+    const oldId = req.params.id;
+    // const newtitle = req.body.title;
+    // const newdes = req.body.des;
+    const { title: newtitle, des: newdes } = req.body;
+    for (let i = 0; i < allitem.length; i++) {
+        if (oldId == allitem[i].id) {
+            allitem[i].title = newtitle;
+            allitem[i].des = newdes;
+            res.json({
+                mesg: "list update",
+                update: allitem[i]
+            });
+            return;
+        }
+    }
+    res.json({
+        mesg: "id not found to update",
+        id: newId,
+    })
+})
+
+app.delete("/delete/:id", function (req, res) {
+    let allItem = db.read();
+    const delId = req.params.id;
+    let arr = [];
+    for (let i = 0, index = 0; i < allItem.length; i++) {
+        if (allItem[i].id != delId) {
+            arr[index] = allItem[i];
+            index++;
+        }
+    }
+    allItem = arr;
+    db.write(allItem)
+    res.json(allItem);
+})
+
 app.listen(3000, () => {
     console.log("server is listening on port 3000 ");
 })
